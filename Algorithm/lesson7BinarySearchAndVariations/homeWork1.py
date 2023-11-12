67c1cb446087ffb0b9150ca85ee89cfbd3c66e7a
Vadim Cherepanov <xenos26@gmail.com>
2023-11-12 21:08:58 +0700

    feat: add solution for lesson 7

---
+++ Algorithm/lesson7BinarySearchAndVariations/homeWork1.py
@@ -0,0 +1,23 @@
+def search_insert(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left
+
+
+n = int(input())
+nums = list(map(int, input().split()))
+target = int(input())
+
+result = search_insert(nums, target)
+print(result)
