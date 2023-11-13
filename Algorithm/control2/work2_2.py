5e28552e535ebe8d507a12ddca4bdc71e7b674c1
Vadim Cherepanov <xenos26@gmail.com>
2023-11-13 22:05:40 +0700

    feat: add versions of work2

---
+++ Algorithm/control2/work2_2.py
@@ -0,0 +1,33 @@
+def exponential_search(arr, needle):
+    n = len(arr)
+    if arr[0] == needle:
+        return [0, 0]
+
+    left = 1
+    right = 1
+    while right < n and arr[right] <= needle:
+        left = right
+        right *= 2
+
+    return binary_search(arr, needle, left, min(right, n - 1))
+
+
+def binary_search(arr, needle, left, right):
+    while left <= right:
+        mid = left + (right - left) // 2
+        if arr[mid] == needle:
+            return [left, right]
+        elif arr[mid] < needle:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return [-1, -1]
+
+
+n = input()
+arr = list(map(int, input().split()))
+needle = int(input())
+result = exponential_search(arr, needle)
+result_str = "".join(map(str, result))
+print(result_str)
