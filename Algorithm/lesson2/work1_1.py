53bbb0a89a8841b300f3397926385c3ae4803a34
Vadim Cherepanov <xenos26@gmail.com>
2023-11-15 21:50:48 +0700

    feat: add solution work1 sort algorithm

---
+++ Algorithm/lesson2/work1_1.py
@@ -0,0 +1,20 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+sorted_arr = insertion_sort(arr)
+
+
+for num in sorted_arr:
+    print(num, end=" ")
